2d01d2b0e1c39f7f2b8d0805f64607d5c047744a
Frederikke <frso0002@stud.kea.dk>
2024-05-27 20:51:06 +0200

    galleri

--- js/galleri.js
+++ js/galleri.js
@@ -1,135 +1,98 @@
-document.querySelector("#galleri_knap").addEventListener("click", changeSlide);
+document.querySelector("#galleri_knap1").addEventListener("click", changeSlide1);
 
-let currentSlide = 1;
+let currentSlide1 = 1;
+let currentSlide2 = 1;
 
-function changeSlide() {
-  console.log("changeSlide", currentSlide);
+function changeSlide1() {
+  console.log("changeSlide", currentSlide1);
+  if (currentSlide1 === 1) {
+    document.querySelector("#galleri_image1").classList.remove("change_image1");
+    document.querySelector("#galleri_image1").classList.add("change_image2");
+    document.querySelector("#galleri_image2").classList.remove("change_image2");
+    document.querySelector("#galleri_image2").classList.add("change_image3");
+    document.querySelector("#galleri_image3").classList.remove("change_image3");
+    document.querySelector("#galleri_image3").classList.add("change_image4");
+    document.querySelector("#galleri_image4").classList.remove("change_image4");
+    document.querySelector("#galleri_image4").classList.add("change_image1");
+    currentSlide1 = 2;
+  } else if (currentSlide1 === 2) {
+    document.querySelector("#galleri_image1").classList.remove("change_image2");
+    document.querySelector("#galleri_image1").classList.add("change_image3");
+    document.querySelector("#galleri_image2").classList.remove("change_image3");
+    document.querySelector("#galleri_image2").classList.add("change_image4");
+    document.querySelector("#galleri_image3").classList.remove("change_image4");
+    document.querySelector("#galleri_image3").classList.add("change_image1");
+    document.querySelector("#galleri_image4").classList.remove("change_image1");
+    document.querySelector("#galleri_image4").classList.add("change_image2");
 
-  if (currentSlide === 1) {
-    document.querySelector("#galleri_img1").classList.remove("change_image1");
-    document.querySelector("#galleri_img1").classList.add("change_image2");
-    document.querySelector("#galleri_img2").classList.remove("change_image2");
-    document.querySelector("#galleri_img2").classList.add("change_image3");
-    document.querySelector("#galleri_img3").classList.remove("change_image3");
-    document.querySelector("#galleri_img3").classList.add("change_image4");
-    document.querySelector("#galleri_img4").classList.remove("change_image4");
-    document.querySelector("#galleri_img4").classList.add("change_image5");
-    document.querySelector("#galleri_img5").classList.remove("change_image5");
-    document.querySelector("#galleri_img5").classList.add("change_image6");
-    document.querySelector("#galleri_img6").classList.remove("change_image6");
-    document.querySelector("#galleri_img6").classList.add("change_image2");
-    currentSlide = 2;
-  } else if (currentSlide === 2) {
-    document.querySelector("#galleri_img1").classList.remove("change_image2");
-    document.querySelector("#galleri_img1").classList.add("change_image3");
-    document.querySelector("#galleri_img2").classList.remove("change_image3");
-    document.querySelector("#galleri_img2").classList.add("change_image4");
-    document.querySelector("#galleri_img3").classList.remove("change_image4");
-    document.querySelector("#galleri_img3").classList.add("change_image5");
-    document.querySelector("#galleri_img4").classList.remove("change_image5");
-    document.querySelector("#galleri_img4").classList.add("change_image6");
-    document.querySelector("#galleri_img5").classList.remove("change_image6");
-    document.querySelector("#galleri_img5").classList.add("change_image1");
-    document.querySelector("#galleri_img6").classList.remove("change_image1");
-    document.querySelector("#galleri_img6").classList.add("change_image3");
-    currentSlide = 3;
-  } else if (currentSlide === 3) {
-    document.querySelector("#galleri_img1").classList.remove("change_image3");
-    document.querySelector("#galleri_img1").classList.add("change_image4");
-    document.querySelector("#galleri_img2").classList.remove("change_image4");
-    document.querySelector("#galleri_img2").classList.add("change_image5");
-    document.querySelector("#galleri_img3").classList.remove("change_image5");
-    document.querySelector("#galleri_img3").classList.add("change_image6");
-    document.querySelector("#galleri_img4").classList.remove("change_image6");
-    document.querySelector("#galleri_img4").classList.add("change_image1");
-    document.querySelector("#galleri_img5").classList.remove("change_image1");
-    document.querySelector("#galleri_img5").classList.add("change_image2");
-    document.querySelector("#galleri_img6").classList.remove("change_image2");
-    document.querySelector("#galleri_img6").classList.add("change_image4");
-    currentSlide = 4;
-  } else if (currentSlide === 4) {
-    document.querySelector("#galleri_img1").classList.remove("change_image4");
-    document.querySelector("#galleri_img1").classList.add("change_image5");
-    document.querySelector("#galleri_img2").classList.remove("change_image5");
-    document.querySelector("#galleri_img2").classList.add("change_image6");
-    document.querySelector("#galleri_img3").classList.remove("change_image6");
-    document.querySelector("#galleri_img3").classList.add("change_image1");
-    document.querySelector("#galleri_img4").classList.remove("change_image1");
-    document.querySelector("#galleri_img4").classList.add("change_image2");
-    document.querySelector("#galleri_img5").classList.remove("change_image2");
-    document.querySelector("#galleri_img5").classList.add("change_image3");
-    document.querySelector("#galleri_img6").classList.remove("change_image3");
-    document.querySelector("#galleri_img6").classList.add("change_image5");
-    currentSlide = 5;
-  } else if (currentSlide === 5) {
-    document.querySelector("#galleri_img1").classList.remove("change_image5");
-    document.querySelector("#galleri_img1").classList.add("change_image6");
-    document.querySelector("#galleri_img2").classList.remove("change_image6");
-    document.querySelector("#galleri_img2").classList.add("change_image1");
-    document.querySelector("#galleri_img3").classList.remove("change_image1");
-    document.querySelector("#galleri_img3").classList.add("change_image2");
-    document.querySelector("#galleri_img4").classList.remove("change_image2");
-    document.querySelector("#galleri_img4").classList.add("change_image3");
-    document.querySelector("#galleri_img5").classList.remove("change_image3");
-    document.querySelector("#galleri_img5").classList.add("change_image4");
-    document.querySelector("#galleri_img6").classList.remove("change_image4");
-    document.querySelector("#galleri_img6").classList.add("change_image6");
-    currentSlide = 6;
-  } else if (currentSlide === 6) {
-    document.querySelector("#galleri_img1").classList.remove("change_image6");
-    document.querySelector("#galleri_img1").classList.add("change_image1");
-    document.querySelector("#galleri_img2").classList.remove("change_image1");
-    document.querySelector("#galleri_img2").classList.add("change_image2");
-    document.querySelector("#galleri_img3").classList.remove("change_image2");
-    document.querySelector("#galleri_img3").classList.add("change_image3");
-    document.querySelector("#galleri_img4").classList.remove("change_image3");
-    document.querySelector("#galleri_img4").classList.add("change_image4");
-    document.querySelector("#galleri_img5").classList.remove("change_image4");
-    document.querySelector("#galleri_img5").classList.add("change_image5");
-    document.querySelector("#galleri_img6").classList.remove("change_image5");
-    document.querySelector("#galleri_img6").classList.add("change_image6");
-    currentSlide = 1;
-  }
+    currentSlide1 = 3;
+  } else if (currentSlide1 === 3) {
+    document.querySelector("#galleri_image1").classList.remove("change_image3");
+    document.querySelector("#galleri_image1").classList.add("change_image4");
+    document.querySelector("#galleri_image2").classList.remove("change_image4");
+    document.querySelector("#galleri_image2").classList.add("change_image1");
+    document.querySelector("#galleri_image3").classList.remove("change_image1");
+    document.querySelector("#galleri_image3").classList.add("change_image2");
+    document.querySelector("#galleri_image4").classList.remove("change_image2");
+    document.querySelector("#galleri_image4").classList.add("change_image3");
+    currentSlide1 = 4;
+  } else if (currentSlide1 === 4) {
+    document.querySelector("#galleri_image1").classList.remove("change_image4");
+    document.querySelector("#galleri_image1").classList.add("change_image1");
+    document.querySelector("#galleri_image2").classList.remove("change_image1");
+    document.querySelector("#galleri_image2").classList.add("change_image2");
+    document.querySelector("#galleri_image3").classList.remove("change_image2");
+    document.querySelector("#galleri_image3").classList.add("change_image3");
+    document.querySelector("#galleri_image4").classList.remove("change_image3");
+    document.querySelector("#galleri_image4").classList.add("change_image4");
+    currentSlide1 = 1;
+}
 }
 
-document.querySelector("#galleri_knap").addEventListener("click", changeSlide);
+document.querySelector("#galleri_knap2").addEventListener("click", changeSlide2);
 
-let currentslide = 1;
+function changeSlide2() {
+  console.log("changeslide", currentSlide2);
 
-function changeslide() {
-  console.log("changeslide", currentslide);
-
-  if (currentSlide === 1) {
+  if (currentSlide2 === 1) {
     document.querySelector("#galleri_img1").classList.remove("change_img1");
     document.querySelector("#galleri_img1").classList.add("change_img2");
     document.querySelector("#galleri_img2").classList.remove("change_img2");
     document.querySelector("#galleri_img2").classList.add("change_img3");
     document.querySelector("#galleri_img3").classList.remove("change_img3");
     document.querySelector("#galleri_img3").classList.add("change_img4");
-    currentslide = 2;
-  } else if (currentslide === 2) {
+    document.querySelector("#galleri_img4").classList.remove("change_img4");
+    document.querySelector("#galleri_img4").classList.add("change_img1");
+    currentSlide2 = 2;
+  } else if (currentSlide2 === 2) {
     document.querySelector("#galleri_img1").classList.remove("change_img2");
     document.querySelector("#galleri_img1").classList.add("change_img3");
     document.querySelector("#galleri_img2").classList.remove("change_img3");
     document.querySelector("#galleri_img2").classList.add("change_img4");
     document.querySelector("#galleri_img3").classList.remove("change_img4");
     document.querySelector("#galleri_img3").classList.add("change_img1");
-    currentslide = 3;
-  } else if (currentslide === 3) {
+    document.querySelector("#galleri_img4").classList.remove("change_img1");
+    document.querySelector("#galleri_img4").classList.add("change_img2");
+    currentSlide2 = 3;
+  } else if (currentSlide2 === 3) {
     document.querySelector("#galleri_img1").classList.remove("change_img3");
     document.querySelector("#galleri_img1").classList.add("change_img4");
     document.querySelector("#galleri_img2").classList.remove("change_img4");
     document.querySelector("#galleri_img2").classList.add("change_img1");
     document.querySelector("#galleri_img3").classList.remove("change_img1");
     document.querySelector("#galleri_img3").classList.add("change_img2");
-    currentslide = 4;
-  } else if (currentslide === 4) {
+    document.querySelector("#galleri_img4").classList.remove("change_img2");
+    document.querySelector("#galleri_img4").classList.add("change_img3");
+    currentSlide2 = 4;
+  } else if (currentSlide2 === 4) {
     document.querySelector("#galleri_img1").classList.remove("change_img4");
     document.querySelector("#galleri_img1").classList.add("change_img1");
     document.querySelector("#galleri_img2").classList.remove("change_img1");
     document.querySelector("#galleri_img2").classList.add("change_img2");
     document.querySelector("#galleri_img3").classList.remove("change_img2");
     document.querySelector("#galleri_img3").classList.add("change_img3");
-    currentslide = 1;
+    document.querySelector("#galleri_img4").classList.remove("change_img3");
+    document.querySelector("#galleri_img4").classList.add("change_img4");
+    currentSlide2 = 1;
   }
 }
